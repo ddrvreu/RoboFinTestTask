01c1c0e5e424583965245797884c798f6863e0f5
3dnnn <ddrvreu@gmail.com>
2021-04-08 08:30:32 +0300

    add readme.md file

---
+++ src/main/java/com/belukha/testtask/CustomerRestService.java
@@ -0,0 +1,12 @@
+package com.belukha.testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRestService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerRestService.class, args);
+    }
+}
